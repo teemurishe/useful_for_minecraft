05617343aa732cba55b0645ac20afb3cb366d62d
timur <matskevich-06@mail.ru>
2021-03-24 14:22:03 +0400

    challenges translated

--- challenges/30 seconds in water challenge.py
+++ challenges/30 seconds in water challenge.py
@@ -2,8 +2,9 @@ import time
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
-mc.postToChat('Приветствуем в челлендже "Продержись под водой 30 секунд!" Цель челленджа - продержаться под водой 30 секунд! За определённые промежутки времени под водой вы будете получать награды. Когда будете готовы, напишите "Начать", чтобы начать выполнение челленджа')
-starter = input("Ожидание команды начала... ")
+mc.postToChat('Welcome to "30 seconds under water challenge"!')
+mc.postToChat('When ready, enter "Start"')
+starter = input("Waiting for the start challenge... ")
 score = 0
 pos = mc.player.getPos()
 blockAbove = mc.getBlock(pos.x, pos.y + 2, pos.z)
@@ -12,20 +13,19 @@ while blockAbove == 8 or blockAbove == 9:
     pos = mc.player.getPos()
     blockAbove = mc.getBlock(pos.x, pos.y + 2, pos.z)
     score = score + 1
-    mc.postToChat("Текущий счет: " + str(score))
-mc.postToChat("Окончательный счет: " + str(score))
+    mc.postToChat("Your score: " + str(score))
+mc.postToChat("Final score: " + str(score))
 if score > 15:
-    mc.postToChat("Поздравляем! Вы продержались под водой уже целых 15 секунд!")
+    mc.postToChat("Congratulations! You have been under water for already 15 seconds!")
     finalPos = mc.player.getTilePos()
     mc.setBlocks(finalPos.x - 5, finalPos.y + 10, finalPos.z - 5,
                  finalPos.x + 5, finalPos.y + 10, finalPos.z + 5, 38)
 if score > 20:
     pos = mc.player.getTilePos
     mc.setBlock(pos.x, pos.y, pos.z, 41)
-    mc.postToChat("Поразительно! Вы смогли продержаться под водой уже 20 секунд!")
+    mc.postToChat("Wow! 20 seconds under water!")
 if score > 30:
     finalPos = mc.player.getTilePos()
     mc.setBlocks(finalPos.x - 5, finalPos.y + 10, finalPos.z - 5,
                  finalPos.x + 5, finalPos.y + 10, finalPos.z + 5, 264)
-    mc.postToChat("Поздравляем! Вы выполнили этот челлендж! Пройдя через такие старания, вы заслуживаете АЛМАЗОВ!!!")
-    
+    mc.postToChat("Hoorah! Challeenge completed! Have some DIAMONDS!")
--- challenges/blocks hitter challenge.py
+++ challenges/blocks hitter challenge.py
@@ -6,25 +6,25 @@ time.sleep(60)
 
 hits = mc.events.pollBlockHits()
 block = 103
-mc.postToChat("Добро пожаловать в новую мини-игру на время! Ваша цель - в течение ровно 1 минуты ударить как можно больше блоков! Затем, телепортируясь, вы узнаете, сколько блоков ударили! Поехали чрез 5, 4, 3...")
+mc.postToChat("Welcome to new mini-game! Hit as many blocks as you can! 5! 4! 3...")
 time.sleep(5)
 for hit in hits:
     x, y, z = hit.pos.x, hit.pos.y, hit.pos.z
     mc.player.setPos(x,y,z)
     time.sleep(10)
-mc.postToChat("Итак, вы ударили ровно" + str(len(hits)) + "раз!")
+mc.postToChat("So, you have hit" + str(len(hits)) + "blocks!")
 if len(hits) < 30:
-    mc.postToChat("Что ж, это не так уж и много, но для первого раза вполне неплохо! Вот тебе приз :)")
+    mc.postToChat("Not bad! This is your prize :)")
     pos = mc.player.getTilePos()
     mc.setBlock(pos.x, pos.y, pos.z, 42)
 if len(hits) < 10:
-    mc.postToChat("Не, ну ты и...! СГОРИ В АДУ!!!")
+    mc.postToChat("Ahahah, lol! Goodbye!")
     mc.setBlock(pos.x, pos.y, pos.z, 90)
 if len(hits) < 50:
-    mc.postToChat("Вау, да ты, видимо, переиграл в куки-кликер! АЛМАЗЫ В СТУДИЮ!!!")
+    mc.postToChat("Wow, hello, cookie-clicker player! Have your diamonds!")
     mc.setBlock(pos.x, pos.y, pos.z, 57)
     mc.setBlock(pos.x, pos.y + 1, pos.z, 57)
 if len(hits) < 80:
-    mc.postToChat("МАКРОСЫ ВЫРУБИЛ!!! На тебе энд, давай, помакроси тут с драконом...")
+    mc.postToChat("Turn off your macrosses and go to The End!")
     mc.setBlock(pos.x, pos.y, pos.z, 119)
-print("Программа остановлена. Благодарим за использование и ждём Вас снова!")
+print("Program stopped. Hope to see you again!")
