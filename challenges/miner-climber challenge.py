4ed313c00cd564f33683593e77387e32368927e8
teemurishe <79781857+teemurishe@users.noreply.github.com>
2021-05-10 18:22:07 +0400

    translated miner-climber challenge

--- challenges/miner-climber challenge.py
+++ challenges/miner-climber challenge.py
@@ -2,9 +2,10 @@ import time
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
-print("Добро пожаловать в шахтёр-горник челлендж! Ваша цель - Спуститься как можно ниже и забраться как можно выше в течение ровно 1 минуты! До начала челленджа осталось 5 секунд!")
+print("Welcome to the miner-climber challenge! Your goal: Climb down and up as low and as high as it's possible during a minute!\
+5 seconds to beginning!")
 time.sleep(10)
-mc.postToChat("Челлендж начинается!")
+mc.postToChat("Let's go!")
 
 heights = [300, -100]
 count = 0
@@ -19,24 +20,24 @@ while count < 60:
     count += 1
     time.sleep(1)
 
-mc.postToChat("Челлендж завершён! Пришло время результатов!")
+mc.postToChat("Challenge finished! Time to discover your results!")
 
 mc.postToChat("Низшая позиция: ",+str(heights[0]))
 mc.postToChat("Высшая позиция: ",+str(heights[1]))
 time.sleep(3)
 
 if heights[0] < -50:
-    mc.postToChat("Да вы прирождённый шахтёр! Ну шахтёр получает по заслугам за свой нелёгкий труд!")
+    mc.postToChat("Wow, you're inborn miner! As you know, good miners get a lot of money...")
     pos = mc.player.getTilePos()
     mc.setBlock(pos.x, pos.y, pos.z, 56)
 elif heights[0] < -100:
-    mc.postToChat("Либо ты читер, либо ты реально шахтёр... Наверное, не надо читеров награждать :)")
+    mc.postToChat("It seems you're CHEATER!... And cheaters don't get prizes :)")
     
 if heights[1] > 50:
-    mc.postToChat("Вы настоящий майнкрафтовский альпинист! Ну а за такое и награды не жалко!")
+    mc.postToChat("You're real climber! Get your climber reward!")
     pos = mc.player.getTilePos()
     mc.setBlock(pos.x, pos.y, pos.z, 80)
     mc.setBlock(pos.x, pos.y + 1, pos.z, 80)
 elif heights[1] > 100:
-    mc.postToChat("СЛЫШ, А НУ ФЛАЙ ОТРУБИ! награду он тут ещё ждёт...")
-print("Челлендж завершён, программа остановлена. Благодарим за использование и ждём Вас снова!")
+    mc.postToChat("TURN OFF YOUR FLY CHEATS! Do you want any reward after that?..")
+print("Challenge finished, program stopped. Thanks for using and see you again!")
