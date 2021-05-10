6f449f27aeb20e9c9f6533f1405db5b2ff9e7e2b
teemurishe <79781857+teemurishe@users.noreply.github.com>
2021-05-10 18:36:36 +0400

    blockplacer translated

--- final/original raw utilies/blockplacer.py
+++ final/original raw utilies/blockplacer.py
@@ -1,7 +1,7 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
-x = int(input("Координата по оси X: "))
-y = int(input("Координата по оси Y: "))
-z = int(input("Координата по оси Z: "))
-id = int(input("ID блока: "))
+x = int(input("X coordinate: "))
+y = int(input("Y coordinate: "))
+z = int(input("Z coordinate: "))
+id = int(input("Block ID: "))
 mc.setBlock(x,y,z, id)
