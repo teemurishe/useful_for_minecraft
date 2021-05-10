ea49ec7c7eea1f371c367685df3d0c1991d1edd3
teemurishe <79781857+teemurishe@users.noreply.github.com>
2021-05-10 18:46:41 +0400

    minetoon's appearance fixed, backslashes added

--- humour/Minetoon the Water God.py
+++ humour/Minetoon the Water God.py
@@ -5,11 +5,11 @@ mc = Minecraft.create()
 count = 1
 while count <= 30:
     pos = mc.player.getPos()
-    mc.postToChat("I am Minetoon, the Water God of Minecraft! I haven't seen
-    your money for so long! Be cursed, people!")
+    mc.postToChat("I am Minetoon, the Water God of Minecraft! I haven't seen\
+    your toll for so long! You're cursed!")
     mc.setBlock(pos.x, pos.y, pos.z, 8)
     count += 2
     time.sleep(1)
 if count >= 30:
-    mc.postToChat("This would be a lesson for you! Don't forget to give money
-    to great gods of Minecraft!")
+    mc.postToChat("This would be a lesson for you! Don't forget to give money\
+    to great Gods of Minecraft!")
