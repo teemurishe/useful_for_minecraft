6e6556faa4b9fbcb02d02d98d2b4085c2acec88f
teemurishe <79781857+teemurishe@users.noreply.github.com>
2021-05-10 17:51:39 +0400

    fix autobuider 2 (backslashes added)

--- Autobuilders/Autobuilder 2 Spinning stairs.py
+++ Autobuilders/Autobuilder 2 Spinning stairs.py
@@ -4,11 +4,11 @@ mc = Minecraft.create()
 
 pos = mc.player.getTilePos()
 
-starter = input('"Autobuilder" started, part 2: "Spinning stairs". Do you want
+starter = input('"Autobuilder" started, part 2: "Spinning stairs". Do you want\
 to continue? Yes/No: ')
 
 if starter == "Yes":
-    print("Please, stand where you want to build stairs. You have 5 seconds
+    print("Please, stand where you want to build stairs. You have 5 seconds\
     before your position would be recorded.")
     time.sleep(5)
     x = pos.x
@@ -17,11 +17,11 @@ if starter == "Yes":
     print("Position recorded succesful! Please, wait...")
     time.sleep(2)
     mc.setBlock(x, y, z+1, 109)
-    povorot = input("Stairs built succesful! Do you want to spin it? Yes/No: ")
-    if povorot == "Yes":
+    turner = input("Stairs built succesful! Do you want to spin it? Yes/No: ")
+    if turner == "Yes":
         mc.setBlock(x, y, z + 1, 109, True)
         print("Program stopped. Hope to see you again!")
-    if povorot =="No":
+    if turner =="No":
         print("Program stopped. Hope to see you again!")
 if starter == "No":
     print("Program stopped. Hope to see you again!")
