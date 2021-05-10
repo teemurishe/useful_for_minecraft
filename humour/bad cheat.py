fdf83dbaef60a756e0c690411c27c054142a94b8
teemurishe <79781857+teemurishe@users.noreply.github.com>
2021-05-10 18:51:11 +0400

    bad cheat translated

--- humour/bad cheat.py
+++ humour/bad cheat.py
@@ -6,10 +6,10 @@ x = pos.x
 y = pos.y
 z = pos.z
 
-nedoalmaz = 10
-answer = input("Congratulations! Cheat activated! Do you want to continue?
+not_a_diamond = 10
+answer = input("Congratulations! Cheat activated! Do you want to continue?\
 (Yes/No): ")
-mc.postToChat("Congratulations! Cheat activated! Do you want to continue?
+mc.postToChat("Congratulations! Cheat activated! Do you want to continue?\
 (Yes/No) Use Python Console to contiue")
 
 if answer == "Yes":
@@ -18,10 +18,10 @@ if answer == "Yes":
     w = int(input("Enter width of diamond ore: "))
     h = int(input("Enter height of diamond ore: "))
     l = int(input("Enter length of diamond ore: "))
-    mc.setBlocks(x, y, z, x+w, y+h, z+l, nedoalmaz)
+    mc.setBlocks(x, y, z, x+w, y+h, z+l, not_a_diamond)
     mc.postToChat("Are you stupid?..")
 elif answer == "No":
-    print("Congratulations, you are not stupid! To proceed acceptance to
-    programmists of Minecraft pay 50 rubles to 4817 7602 2418 2786 card")
+    print("Congratulations, you are not stupid! To proceed acceptance to\
+    programmists of Minecraft transfer 1 dollar to 4817 7602 2418 2786 card")
 else:
     print("Incorrect choice. Please, try again. Program stopped!")
